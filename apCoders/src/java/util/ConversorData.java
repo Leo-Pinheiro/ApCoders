327f906dcea108f0fb0185000c0fd4121223ff59
Leonardo Luiz Moreira Pinheiro <52048106+Leo-Pinheiro@users.noreply.github.com>
2022-01-23 12:15:44 -0300

    Delete apCoders directory

--- apCoders/src/java/util/ConversorData.java
+++
@@ -1,9 +0,0 @@
-package util;
-import java.util.Calendar;
-
-public class ConversorData {
-    public static java.sql.Date conv(Calendar d){
-       java.sql.Date data = new java.sql.Date(d.getTime().getTime());
-       return data;
-    }
-}
